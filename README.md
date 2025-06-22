1bc69549aa60a30b09deefcdcbcd1a8bf4954989
Raghav Kalyan <1037059@lwsd.org>
2025-06-22 10:21:06 -0700

    initial commit for rehaul

--- README.md
+++
@@ -1,5 +0,0 @@
-View website at https://www.raghavkalyan.com/
-
-Template taken from [[Astrofy](https://astrofy-template.netlify.app)](https://github.com/manuelernestog/astrofy)
-
-
--- src/config.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Raghav Kalyan | Portfolio';
-export const SITE_DESCRIPTION = "Hi, I'm Raghav Kalyan. I'm currently a junior attending Tesla Stem High School in Redmond, Washington, with lots of interest in Data Science, Machine Learning, and Game Development.";
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
